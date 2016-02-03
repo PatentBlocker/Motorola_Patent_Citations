44c58e846aec9f971890f2bd961caaa8d7353c08
Xilin Sun <s.sn.giraffe@gmail.com>
2016-02-02 22:43:02 -0800

    Add scripts and data sets for blockings without duplicates

--- src/Harry_get_Citations_BlockingApps.py
+++ src/Harry_get_Citations_BlockingApps.py
@@ -6,12 +6,16 @@ import pandas as pd
 import matplotlib as plt
 
 Blocking = pd.read_csv("../data/Motorola_blocking_patents_1114.csv")
-MotoPatents = pd.read_csv("../data/Motorola_Patents_Blocking_Citations.csv", index=False)
+MotoPatents = pd.read_csv("../data/Motorola_Patents_Blocking_Citations.csv")
 
 del Blocking['Unnamed: 0']
 
+Blocking['Blocking_patent_str'] = ""
+
 for i in range(0, len(Blocking)):
 	Blocking['Blocking_patent_str'][i] = str(Blocking['Blocking_patent'][i])
+
+MotoPatents['Blocking_appnumber'] = 0
 
 for i in range(0, len(MotoPatents)):
 	MotoPatents['Blocking_appnumber'][i] = len(set(Blocking['Blocked_app'][Blocking['Blocking_patent_str']==MotoPatents['PatentID'][i]]))
